4347ca0e514ece1a41b258e7d810a02bfe7e2424
Damien SOREL <damien.sorel@sogelink.fr>
2022-12-30 13:21:29 +0100

    chore: update rollup

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,8 +1,8 @@
-import babel from 'rollup-plugin-babel';
-import replace from 'rollup-plugin-replace';
-import commonjs from 'rollup-plugin-commonjs';
+import { babel } from '@rollup/plugin-babel';
+import replace from '@rollup/plugin-replace';
+import commonjs from '@rollup/plugin-commonjs';
 
-const pkg = require('./package.json');
+import pkg from './package.json' assert { type: 'json' };
 
 export default {
     input   : 'index.js',
